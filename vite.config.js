06be94bbe9cc8368e3c96e8266e13c6b2e3f118b
Cahyono <142607246+yono99@users.noreply.github.com>
2024-12-24 04:27:13 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    build: {
+        chunkSizeWarningLimit: 1600,
+      },
     plugins: [
         laravel({
             input: 'resources/js/app.js',
